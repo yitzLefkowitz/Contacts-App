ccccd2d754f67464c8c43fd255de4be46c0f1d1f
Yitz <dlefkowitz@lightricks.com>
2022-06-14 15:50:22 +0300

    Add gradle deps

--- app/src/main/java/com/lighricks/contactsapp/MainActivity.kt
+++ app/src/main/java/com/lighricks/contactsapp/MainActivity.kt
@@ -3,14 +3,8 @@ package com.lighricks.contactsapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.*
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
-import com.lighricks.contactsapp.ui.theme.AppBarBackground
 import com.lighricks.contactsapp.ui.theme.ContactsAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,11 +14,6 @@ class MainActivity : ComponentActivity() {
             App()
         }
     }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
 }
 
 @Preview(showBackground = true)
