3bff96226fb97456033b9bbe6ee9a5f85e2b5927
Yitz <dlefkowitz@lightricks.com>
2022-06-15 14:42:40 +0300

    Create contacts cache

---
+++ app/src/main/java/com/lighricks/contactsapp/data/ContactsCache.kt
@@ -0,0 +1,21 @@
+package com.lighricks.contactsapp.data
+
+import kotlinx.coroutines.flow.*
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class ContactsCache @Inject constructor() {
+
+    private val contacts = MutableStateFlow<Map<Long, ContactData>>(emptyMap())
+
+    fun updateContacts(newContacts: List<ContactData>) {
+        contacts.value = newContacts.associateBy { it.id }
+    }
+
+    fun getContacts(): Flow<Collection<ContactData>> = contacts.map { it.values }
+
+    fun getContact(id: Long): Flow<ContactData> {
+        return contacts.mapNotNull { it[id] }
+    }
+}
